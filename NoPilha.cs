3a5f28b96dae3d364b33e6431433a40b1f4b1d88
Guilherme Gomes leal <guilhermegomesleal@hotmail.com>
2020-09-29 14:31:00 -0300

    pia

---
+++ NoPilha.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitivasGráficas
+{
+    class NoPilha
+    {
+        private Ponto2 p;
+        private NoPilha prox;
+
+      
+        public NoPilha(Ponto2 p, NoPilha prox)
+        {
+            this.p = p;
+            this.prox = prox;
+        }
+
+        public NoPilha()
+        {
+
+        }
+
+        public Ponto2 P { get => p; set => p = value; }
+        public NoPilha Prox { get => prox; set => prox = value; }
+
+    }
+}
---
+++ PilhaCor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitivasGráficas
+{
+    class PilhaCor
+    {
+        private NoPilha inicio;
+
+        public PilhaCor()
+        {
+            this.inicio = null;
+        }
+
+        public Ponto2 retirar()
+        {
+            Ponto2 aux = inicio.P;
+            inicio = inicio.Prox;
+
+            return aux;
+        }
+        
+        public void inserir(Ponto2 pont)
+        {
+            NoPilha novo = new NoPilha(pont, inicio);
+            inicio = novo;
+        }
+
+    }
+}
